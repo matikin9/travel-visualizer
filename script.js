943b7e0c3a849ec9f69b378cc2997db8acf14fa0
Nina Kin <matikin9@gmail.com>
2024-10-15 01:16:02 -0700

    add activities

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
-const DATA_SOURCE = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQVEanmT6_1IXNFstVxsplwm9iAGV5SQ7AzlFQce0_9e8YmvGCTt5DGbWMJCmbKOCv2XQZj-sShq2p9/pub?output=csv';
+const DATA_SOURCE_STAYS = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQVEanmT6_1IXNFstVxsplwm9iAGV5SQ7AzlFQce0_9e8YmvGCTt5DGbWMJCmbKOCv2XQZj-sShq2p9/pub?gid=0&single=true&output=csv';
+const DATA_SOURCE_ACTIVITES = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQVEanmT6_1IXNFstVxsplwm9iAGV5SQ7AzlFQce0_9e8YmvGCTt5DGbWMJCmbKOCv2XQZj-sShq2p9/pub?gid=69921073&single=true&output=csv';
 
-// fetch data from google sheets
-fetch(DATA_SOURCE)
-  .then(response => response.text())
-  .then(text => {
-    const data = Papa.parse(text, { header: true }).data;
-    console.log(data);
-    displayData(data);
-});
+// fetch data from multiple data sources
+// Promise.all([
+//     fetch(DATA_SOURCE_STAYS).then(response => response.text()),
+//     fetch(DATA_SOURCE_ACTIVITES).then(response => response.text())
+// ])
+
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const data = await response.text();
+        console.log(data);
+        
+        return data;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+Promise.all([getData(DATA_SOURCE_STAYS), getData(DATA_SOURCE_ACTIVITES)])
+    .then(results => {
+        let stays = results[0];
+        let activities = results[1];
+
+        let stayData = Papa.parse(stays, { header: true }).data;
+        let activityData = Papa.parse(activities, { header: true }).data;
+
+        displayData(stayData, activityData);
+    })
+    .catch(err => console.error(err));
 
 let iDate = 0;
 let iLocale = 0;
 let lastLocale = '';
 let dateCounter;
 
-function displayData(data) {
+function displayData(stayData, activityData) {
     const itinerary = document.querySelector('#itinerary');
 
     // loop through data and display on page
-    data.forEach(row => {
-        if (lastLocale != '' && lastLocale != row['Locale']) {
+    stayData.forEach(stayRow => {
+        if (lastLocale != '' && lastLocale != stayRow['Locale']) {
             iLocale++;
         }
 
         if (iDate == 0) {
             // initialize with 2 columns
-            dateCounter = new Date(row['StartDate']);
+            dateCounter = new Date(stayRow['Date']);
             createColumn(dateCounter);
             
             iDate++;
@@ -38,22 +64,29 @@ function displayData(data) {
     
         const div = document.createElement('div');
         div.classList.add('card');
-        div.innerHTML = `${row['Locale']}<br>${row['Notes']}`;
+        div.innerHTML = `<strong>${stayRow['Locale']}</strong><br>${stayRow['Stay']}<br>${stayRow['Notes']}`;
         div.style.left = `${((iDate-1) * 230) + 120}px`;
         div.style.top = `${(iLocale * 120)}px`;
 
+        const divChild = document.createElement('div');
+        divChild.classList.add('card-activities');
+        
+        // loop through activities and display on page
+        activityData.forEach(activityRow => {
+            if (activityRow['Date'] == stayRow['Date']) {
+                divChild.innerHTML += `${activityRow['Activity']}<br>`;
+            }
+        });
+
+        let divchildHeight = divChild.clientHeight;
+        div.appendChild(divChild);
+
         itinerary.appendChild(div);
 
-        // log x and y position of card within page
-        console.log(`Locale: ${row['Locale']} | Notes: ${row['Notes']} | x: ${((iDate-1) * 230) + 120}px | y: ${(iLocale * 120)}px`);
-        
-        // log height of itinerary div
-        console.log(itinerary.clientHeight);
-
         // set html height to the y position of the last card plus its height
-        document.querySelector('#background').style.height = `${(iLocale * 120) + 280}px`;
+        document.querySelector('#background').style.height = `${(iLocale * 120) + 280 + divchildHeight + 40}px`;
         
-        lastLocale = row['Locale'];
+        lastLocale = stayRow['Locale'];
         iDate++;
     });
 }
@@ -61,8 +94,9 @@ function displayData(data) {
 function createColumn(date) {
     const background = document.querySelector('#background');
     const bgColumn = document.createElement('div');
+    const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-    let formattedDate = date.toLocaleString('en-US', { month: 'short', day: 'numeric' });
+    let formattedDate = dayOfWeek[date.getDay()] + ', ' +  (date.getMonth() + 1) + '/' + date.getDate();
 
     bgColumn.classList.add('bg-column');
     bgColumn.style.left = `${iDate * 230}px`;
